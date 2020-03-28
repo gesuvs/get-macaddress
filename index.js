7d5f3be9ba7553e1cd13d147c74ed74c2d98ae69
GBernardo10 <g_bernardo10@hotmail.com>
2020-03-28 02:20:15 -0300

    deploy heroku

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const axios = require("axios");
 const macaddress = require("macaddress");
 const app = express();
 
-app.get("/", (req, res) => {
+const state = {};
+
+app.get("/", async (req, res) => {
+  await axios.default.get("https://api.ipify.org/?format=json").then(result => {
+    state.ip = result.data.ip;
+  });
   macaddress.one(function(err, mac) {
-    res.json({ mac });
+    state.mac = mac;
+    //res.json({ state });
+    res.send(`
+    <body style="display:flex; justify-content:center; align-items:center">
+    <div>
+      <p>ENDEREÇO IP: ${state.ip}</p>
+      <p>ENDEREÇO MAC: ${state.mac}</p>
+    </div>
+    </body>
+    `);
   });
 });
 
